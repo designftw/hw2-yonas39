79ef1f7ca0d4d4fd6c014543eefccb53b4add3eb
yonas39 <124627215+yonas39@users.noreply.github.com>
2023-02-23 00:04:35 -0500

    expenses draft

--- expenses/index.js
+++ expenses/index.js
@@ -1,24 +1,40 @@
 import { createApp } from "https://mavue.mavo.io/mavue.js";
 
-globalThis.app = createApp({
-	data: {
-		expenses: []
-	},
+globalThis.app = createApp(
+  {
+    data: {
+      expenses: [],
+    },
 
-	computed: {
-		total_balance () {
-			let total = 0;
+    computed: {
+      total_balance() {
+        let total = 0;
 
-			for (let expense of this.expenses) {
-				let trinity_paid = expense.trinity_paid ?? 0;
-				let neo_paid = expense.neo_paid ?? 0;
-				let trinity_paid_for_neo = expense.trinity_paid_for_neo ?? 0;
-				let neo_paid_for_trinity = expense.neo_paid_for_trinity ?? 0;
+        for (let expense of this.expenses) {
+          let trinity_paid = expense.trinity_paid ?? 0;
+          let neo_paid = expense.neo_paid ?? 0;
+          let trinity_paid_for_neo = expense.trinity_paid_for_neo ?? 0;
+          let neo_paid_for_trinity = expense.neo_paid_for_trinity ?? 0;
 
-				total += (trinity_paid - neo_paid)/2 + trinity_paid_for_neo - neo_paid_for_trinity;
-			}
+          total +=
+            (trinity_paid - neo_paid) / 2 +
+            trinity_paid_for_neo -
+            neo_paid_for_trinity;
+        }
 
-			return total;
-		}
-	}
-}, "#app");
+        return total;
+      },
+    },
+    methods: {
+      addExpense() {
+        this.expenses.push({
+          neo_paid: 0,
+          trinity_paid: 0,
+          trinity_paid_for_neo: 0,
+          neo_paid_for_trinity: 0,
+        });
+      },
+    },
+  },
+  "#app"
+);
